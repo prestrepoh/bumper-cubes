03baa2ee69804fd24adba55141f354b4db1b9565
Principal\Pablo <prestrepoh>
2017-01-21 17:56:57 -0500

    v 1.0.0 -Added a movement script for Player 1 -Added rotation to PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed;
+	public float rotationSpeed;
+
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		//Player rotation
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+		rigidBody.AddTorque(transform.up * rotation);
+	}
+}
