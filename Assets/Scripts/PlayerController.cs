62f4ee9165833f88c26ffa9186c52c6a69ed93ed
Principal\Pablo <prestrepoh>
2017-01-22 10:01:09 -0500

    -Completed player's movement script -Added a physics material to player and set bounciness to 1 -Added notes.md file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,26 @@ public class PlayerController : MonoBehaviour {
 
 	public float movementSpeed;
 	public float rotationSpeed;
+	public GameObject playerFront;
 
 	private Rigidbody rigidBody;
+	private Vector3 playerDirection;
+	private float playerRotation;
+	private float playerMovement;
 
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 	}
-
+		
 	void FixedUpdate ()
 	{
 		//Player rotation
-		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-		rigidBody.AddTorque(transform.up * rotation);
+		playerRotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+		rigidBody.AddTorque(transform.up * playerRotation);
+
+		//Player movement
+		playerDirection = Vector3.Normalize(playerFront.transform.position - transform.position);
+		playerMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		rigidBody.AddForce (playerDirection * playerMovement);
 	}
 }
---
+++ Notes.md
@@ -0,0 +1,5 @@
+Notes:
+-To change player's movement style, we can modify: 
+	-player's speed global variable
+	-player's rigid body drag
+	-player's rigid body angular drag
