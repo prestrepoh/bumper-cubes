b094f8f9bde0fbdcb79bc5d809806e968367238f
Principal\Pablo <prestrepoh>
2017-01-23 19:17:53 -0500

    Added Managers for the goal zones

---
+++ Assets/Scripts/Goal Zones/GoalZone2Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZone2Manager : MonoBehaviour {
+
+	public GameObject ball;
+	SceneManager sceneManager = new SceneManager ();
+
+	void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject == ball){
+			sceneManager.scoreGoalForPlayer1();
+		}
+	}
+}
