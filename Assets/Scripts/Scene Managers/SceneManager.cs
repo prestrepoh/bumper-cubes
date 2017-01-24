b5e49e749915db5605aab8d1ad1a34f2df0ad1eb
Principal\Pablo <prestrepoh>
2017-01-23 19:13:21 -0500

    Added SceneManager class

---
+++ Assets/Scripts/Scene Managers/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager{
+
+	private int player1Score = 0;
+	private int player2Score = 0;
+
+	public void scoreGoalForPlayer1(){
+		this.player1Score++;
+	}
+
+	public void scoreGoalForPlayer2(){
+		this.player2Score++;
+	}
+}
