4a19f3fc1059a22d415d189966181a08796dfce1
Principal\Pablo <prestrepoh>
2017-01-23 19:11:13 -0500

    Renamed Player Controller to PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
 
 	public float movementSpeed;
 	public float rotationSpeed;
