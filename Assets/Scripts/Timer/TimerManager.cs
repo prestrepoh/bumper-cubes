71663543662ba22a0b768c8309acb10f0f0eee29
Principal\Pablo <prestrepoh>
2017-01-26 15:30:51 -0500

    Added a working time manager that interacts with the scene manager

---
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour {
+
+	public float timeLeft;
+	public GameObject timerText;
+	public GameObject sceneManager;
+
+	private SceneManager sceneManagerScript;
+
+	void Start(){
+		sceneManagerScript = sceneManager.GetComponent<SceneManager> ();
+	}
+
+	void Update () {
+		
+		timeLeft -= Time.deltaTime; 
+
+		if (timeLeft <= 0) {
+			timeLeft = 0;
+			sceneManagerScript.timeUp ();
+		} 
+
+		timerText.GetComponent<Text>().text = timeLeft.ToString();
+	}
+}
